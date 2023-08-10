4a2559bb91f69b8522bba96ae0835ad15231a607
zaritskiyaa <zaritskiyaa@mail.ru>
2023-08-11 00:24:15 +0300

    fixed operand NoneType

--- homework.py
+++ homework.py
@@ -33,29 +33,26 @@ HOMEWORK_VERDICTS: Dict[str, str] = {
 }
 EXCEPTIONS_MESSAGE: Dict[str, str] = {
     MessageError: 'Cбой при отправке сообщения в Telegram',
-    requests.exceptions.RequestException: 'Возникла проблема с запросом',
+    requests.exceptions.RequestException:
+    'Возникла проблема с запросом',
     UnexpectedStatusError: f'Недоступен {ENDPOINT}.',
     JSONDecodeError: 'Возникла проблема с декодировкой json',
     TypeError: 'Тип данных API не соотвествует',
     KeyError: 'Ошибка с ключами "homework_name" или  "status"',
 }
 ENV_TOKENS: List[str] = [
-    'PRACTICUM_TOKEN',
-    'TELEGRAM_TOKEN',
-    'TELEGRAM_CHAT_ID',
-    'ENDPOINT',
+    'PRACTICUM_TOKEN', 'TELEGRAM_TOKEN',
+    'TELEGRAM_CHAT_ID', 'ENDPOINT',
 ]
-
 
 def check_tokens() -> bool:
     """Убеждаемся что все данные окружения присуствуют."""
     uncorrect_token: List[str] = list(
-        filter(lambda d: isinstance(globals().get(d), type(None)), ENV_TOKENS)
+        filter(lambda d: not globals().get(d), ENV_TOKENS)
     )
     if uncorrect_token:
         logging.critical(
-            f'Отсутствие обязательных переменных окружения {uncorrect_token}'
-        )
+            f'Отсутствие обязательных переменных окружения {uncorrect_token}')
         return False
     return True
 
@@ -93,18 +90,21 @@ def get_api_answer(timestamp: int) -> Dict[str, Any]:
     return response
 
 
-def check_response(response: Dict[str, Any]) -> List:
+def check_response(response: Dict[str, Any]) -> List | List[Dict[str, Any]]:
     """Валидируем полученные данные от API."""
     if not isinstance(response, dict):
         logging.info(f'Тип данных {type(response)}')
         raise TypeError(f'Тип данных API {type(response)} != <dict>')
-
-    homeworks: List = response.get("homeworks")
+    
+    homeworks: List[None] | List[Dict[str, Any]] = response.get("homeworks")
 
     if not isinstance(homeworks, list):
-        logging.info(f'Тип данных  ключа "homeworks" {type(homeworks)}')
+        logging.info(
+            f'Тип данных  ключа "homeworks" {type(homeworks)}'
+        )
         raise TypeError(
-            'Тип данных по ключу "homeworks" ' f'{type(homeworks)} != <list>'
+            'Тип данных по ключу "homeworks" '
+            f'{type(homeworks)} != <list>'
         )
     elif not response.get('current_date'):
         raise TypeError(
@@ -122,9 +122,7 @@ def parse_status(homework: Dict[str, Any]) -> str:
     verdict: str = HOMEWORK_VERDICTS.get(homework.get('status'))
     if homework.get('status') not in HOMEWORK_VERDICTS.keys():
         logging.info(f'статус домашки {homework.get("status")}')
-        raise ValueError(
-            f'Неожиданный статус домашки {homework.get("status")}'
-        )
+        raise ValueError(f'Неожиданный статус домашки {homework.get("status")}')
     elif 'homework_name' not in homework.keys():
         raise KeyError('В ответе API отсутсвует название домашки')
     return f'Изменился статус проверки работы "{name}". {verdict}'
@@ -143,9 +141,7 @@ def main() -> NoReturn:
     while True:
         try:
             response: Dict[str, Any] = get_api_answer(timestamp)
-            answer_server: List = check_response(
-                response
-            )
+            answer_server: List[None] | List[Dict[str, Any]] = check_response(response)
             timestamp: int = response['current_date']
             if answer_server:
                 send_message(bot, message=parse_status(answer_server[0]))
@@ -175,10 +171,7 @@ if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.DEBUG,
-        handlers=[
-            logging.FileHandler(LOG_FILE_DIR, encoding='UTF-8'),
-            logging.StreamHandler(sys.stdout),
-        ],
-    )
-
+        handlers=[logging.FileHandler(LOG_FILE_DIR, encoding='UTF-8'),
+                  logging.StreamHandler(sys.stdout)])
+    
     main()
